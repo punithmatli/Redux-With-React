7c01dd3b15de9261ac4f27d74629de3350bb8602
unknown <punithmatli@gmail.com>
2020-06-01 13:13:33 +0530

    Added redux-thunk to handle asynchronous code

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import * as actionTypes from '../../store/actions'
+import * as actionCreators from '../../store/actions/actions'
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    render () {
+    render() {
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
-                <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
-                <CounterControl label="Subtract 8" clicked={this.props.onSubtractCounter}  />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
+                <CounterControl label="Add 10" clicked={this.props.onAddCounter} />
+                <CounterControl label="Subtract 8" clicked={this.props.onSubtractCounter} />
                 <hr />
                 <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
                 <ul>
                     {this.props.storedResults.map(strResult => (
                         <li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>
-                    ))}     
+                    ))}
                 </ul>
-            
+
             </div>
         );
     }
@@ -29,20 +29,20 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr:state.ctr.counter,
-        storedResults:state.res.results
+        ctr: state.ctr.counter,
+        storedResults: state.res.results
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({type:actionTypes.INCREMENT}),
-        onDecrementCounter: () => dispatch({type:actionTypes.DECREMENT}),
-        onAddCounter: () => dispatch({type:actionTypes.ADD, val:10}),
-        onSubtractCounter: () => dispatch({type:actionTypes.SUBTRACT,val:8}),
-        onStoreResult: (result) => dispatch({type:actionTypes.STORE_RESULT, result:result}),
-        onDeleteResult: (id) => dispatch({type:actionTypes.DELETE_RESULT, resultElId:id})
+        onIncrementCounter: () => dispatch(actionCreators.increment()),
+        onDecrementCounter: () => dispatch(actionCreators.decrement()),
+        onAddCounter: () => dispatch(actionCreators.add(10)),
+        onSubtractCounter: () => dispatch(actionCreators.subtract(8)),
+        onStoreResult: (result) => dispatch(actionCreators.storeResult(result)),
+        onDeleteResult: (id) => dispatch(actionCreators.deleteResult(id))
     }
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(Counter);
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,7 +1,7 @@
-import * as actionTypes from '../actions'
+import * as actionTypes from '../actions/actions'
 
 const initialState = {
-    counter:0
+    counter: 0
 }
 
 const reducer = (state = initialState, action) => {
@@ -9,22 +9,22 @@ const reducer = (state = initialState, action) => {
         case actionTypes.INCREMENT:
             return {
                 ...state,
-                counter:state.counter+1
+                counter: state.counter + 1
             }
         case actionTypes.DECREMENT:
             return {
                 ...state,
-                counter:state.counter-1
+                counter: state.counter - 1
             }
         case actionTypes.ADD:
             return {
                 ...state,
-                counter:state.counter+action.val
+                counter: state.counter + action.val
             }
         case actionTypes.SUBTRACT:
             return {
                 ...state,
-                counter:state.counter-action.val
+                counter: state.counter - action.val
             }
     }
     return state;
